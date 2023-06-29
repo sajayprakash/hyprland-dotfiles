0112ac37dad7144009360921a8de36fb4afb3bc4
sajayprakash <sajayprakash7@protonmail.com>
2023-06-29 19:38:44 +0530

    add copydots script

---
+++ copydots.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Configs
+directories=(
+    ~/.config/mako
+    ~/.config/wofi
+    ~/.config/hypr
+    ~/.config/hypr
+    ~/.config/firefox
+    ~/.config/foot
+    ~/.config/git
+    ~/.config/paru
+    ~/.config/wallpapers
+    ~/.config/fish/
+)
+
+# Loop through each directory
+for dir in "${directories[@]}"
+do
+    # Copy directory contents to the current directory
+    cp -R "$dir" .
+done
+
